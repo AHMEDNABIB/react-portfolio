5eaa2c5b0c7cea7e20ea02f1405628d740476a63
AHMEDNABIB <nabibpallab@gmail.com>
2023-12-26 02:29:19 +0600

    refactor

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,9 +18,7 @@ const Navbar = () => {
 
 	return (
 		<div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
-			<div>
-				<img src={Logo} alt="Logo Image" style={{ width: "200px" }} />
-			</div>
+			<h3 className="text-3xl font-semibold text-pink-600">AN</h3>
 
 			{/* menu */}
 			<ul className="hidden md:flex">
