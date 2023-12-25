dd794c6c07f5d47763f2f209060b2f7a91595afe
AHMEDNABIB <nabibpallab@gmail.com>
2023-12-26 02:54:23 +0600

    refactor

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { HiArrowNarrowRight } from "react-icons/hi";
+import { Link } from "react-scroll";
 
 const Home = () => {
 	return (
@@ -14,18 +15,21 @@ const Home = () => {
 					I'm a Full Stack Developer.
 				</h2>
 				<p className="text-[#8892b0] py-4 max-w-[700px]">
-					I’m a full-stack developer specializing in building exceptional digital experiences.
-					Currently, I’m focused on building responsive full-stack web
-					applications.
+					I’m a full-stack developer specializing in building
+					exceptional digital experiences. Currently, I’m focused on
+					building responsive full-stack web applications.
 				</p>
-				<div>
-					<button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
-						View Work
-						<span className="group-hover:rotate-90 duration-300">
-							<HiArrowNarrowRight className="ml-3 " />
-						</span>
-					</button>
-				</div>
+
+				<Link to="work" smooth={true} duration={500}>
+					<div>
+						<button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
+							View Work
+							<span className="group-hover:rotate-90 duration-300">
+								<HiArrowNarrowRight className="ml-3 " />
+							</span>
+						</button>
+					</div>
+				</Link>
 			</div>
 		</div>
 	);
