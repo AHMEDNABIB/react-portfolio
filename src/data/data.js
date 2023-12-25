0eb27398af67e99dd56c23edd9cbb2158285ce05
AHMEDNABIB <nabibpallab@gmail.com>
2023-12-26 02:51:46 +0600

    refactor

--- src/data/data.js
+++ src/data/data.js
@@ -1,35 +1,29 @@
-import WorkIm from '../assets/projects/workImg.jpeg'
-import RealEst from '../assets/projects/realestate.jpg'
 
-export const data=[
-    {
-        id:1,
-        name:"React JS Application",
-        image:WorkIm,
-        github:"",
-        live:"",
-    },
-    {
-        id:2,
-        name:"React JS Application",
-        image:RealEst,
-        github:"",
-        live:"",
-    },
-    {
-        id:3,
-        name:"React JS Application",
-        image:WorkIm,
-        github:"",
-        live:"",
-    },
-    {
-        id:4,
-        name:"React JS Application",
-        image:RealEst,
-        github:"",
-        live:"",
-    },
+import Food from '../assets/projects/food.jpg'
+import Wild from '../assets/projects/wild.jpg'
+import Recipe from '../assets/projects/recipe.jpg'
 
 
-]
+export const data = [
+	{
+		id: 1,
+		name: "MERN Stack Application",
+		image: Food,
+		github: "https://github.com/AHMEDNABIB/react-restaurants-client",
+		live: "https://react-restaurant-123.netlify.app/",
+	},
+	{
+		id: 2,
+		name: "React JS Application",
+		image: Wild,
+		github: "https://github.com/AHMEDNABIB/the-wild-oasis",
+		live: "https://wild-oasis-1234.netlify.app/login",
+	},
+	{
+		id: 3,
+		name: "Javascript Application",
+		image: Recipe,
+		github: "https://github.com/AHMEDNABIB/food-reciepe",
+		live: "https://food-recipe-hut.netlify.app/#5ed6604591c37cdc054bca3b",
+	},
+];
